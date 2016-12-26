aee9792515a015e0647aa071874e1e40278ba88f
Reuben Morais <reuben.morais@gmail.com>
2016-12-26 17:41:36 -0200

    Aggregate transcripts into a CSV file

--- util/importers/lapsbm.py
+++ util/importers/lapsbm.py
@@ -5,6 +5,7 @@ import unicodedata
 import codecs
 import fnmatch
 import os
+import pandas
 
 from glob import glob
 from math import ceil
@@ -41,7 +42,7 @@ class DataSets(object):
         return self._test
 
 class DataSet(object):
-    def __init__(self, txt_files, thread_count, batch_size, numcep, numcontext):
+    def __init__(self, filelist, thread_count, batch_size, numcep, numcontext):
         self._numcep = numcep
         self._x = tf.placeholder(tf.float32, [None, numcep + (2 * numcep * numcontext)])
         self._x_length = tf.placeholder(tf.int32, [])
@@ -51,7 +52,7 @@ class DataSet(object):
                                                   dtypes=[tf.float32, tf.int32, tf.int32, tf.int32],
                                                   capacity=2 * self._get_device_count() * batch_size)
         self._enqueue_op = self._example_queue.enqueue([self._x, self._x_length, self._y, self._y_length])
-        self._txt_files = txt_files
+        self._filelist = filelist
         self._batch_size = batch_size
         self._numcontext = numcontext
         self._thread_count = thread_count
@@ -68,24 +69,18 @@ class DataSet(object):
             batch_thread.start()
 
     def _create_files_circular_list(self):
-        priorityQueue = PriorityQueue()
-        for txt_file in self._txt_files:
-          wav_file = os.path.splitext(txt_file)[0] + ".wav"
-          wav_file_size = os.path.getsize(wav_file)
-          priorityQueue.put((wav_file_size, (txt_file, wav_file)))
-        files_list = []
-        while not priorityQueue.empty():
-            priority, (txt_file, wav_file) = priorityQueue.get()
-            files_list.append((txt_file, wav_file))
-        return cycle(files_list)
+        # 1. Sort by wav filesize
+        # 2. Select just wav filename and transcript columns
+        # 3. Create a cycle
+        return cycle(self._filelist.sort_values(by="wav_filesize")
+                                   .ix[:, ["wav_filename", "transcript"]]
+                                   .itertuples(index=False))
 
     def _populate_batch_queue(self, session):
-        for txt_file, wav_file in self._files_circular_list:
+        for wav_file, transcript in self._files_circular_list:
             source = audiofile_to_input_vector(wav_file, self._numcep, self._numcontext)
             source_len = len(source)
-            with codecs.open(txt_file, encoding="utf-8") as open_txt_file:
-                target = unicodedata.normalize("NFKD", open_txt_file.read().strip()).encode("ascii", "ignore")
-                target = text_to_char_array(target)
+            target = text_to_char_array(transcript)
             target_len = len(target)
             try:
                 session.run(self._enqueue_op, feed_dict={
@@ -103,32 +98,59 @@ class DataSet(object):
 
     @property
     def total_batches(self):
-        # Note: If len(_txt_files) % _batch_size != 0, this re-uses initial _txt_files
-        return int(ceil(float(len(self._txt_files)) /float(self._batch_size)))
+        # Note: If len(_filelist) % _batch_size != 0, this re-uses initial files
+        return int(ceil(float(len(self._filelist)) /float(self._batch_size)))
 
 
 def read_data_sets(data_dir, train_batch_size, dev_batch_size, test_batch_size, numcep, numcontext, thread_count=8, limit_dev=0, limit_test=0, limit_train=0):
-    # Conditionally download data
-    LAPS_DATA = "LapsBM1.4.rar"
-    LAPS_DATA_URL = "http://www.laps.ufpa.br/falabrasil/files/LapsBM1.4.rar"
-    local_file = base.maybe_download(LAPS_DATA, data_dir, LAPS_DATA_URL)
+    # Read the processed set files from disk if they exist, otherwise create 
+    # them.
+    train_files = None
+    train_csv = os.path.join(data_dir, "lapsbm-train.csv")
+    if gfile.Exists(train_csv):
+        train_files = pandas.read_csv(train_csv)
 
-    # Conditionally extract data
-    LAPS_DIR = "LapsBM1.4"
-    _maybe_extract(data_dir, LAPS_DIR, local_file)
+    dev_files = None
+    dev_csv = os.path.join(data_dir, "lapsbm-dev.csv")
+    if gfile.Exists(dev_csv):
+        dev_files = pandas.read_csv(dev_csv)
 
-    # Conditionally split data into train/validation/test sets
-    _maybe_split_sets(data_dir, LAPS_DIR, "laps-sets")
-    
-    # Create dev DataSet
-    work_dir = os.path.join(data_dir, "laps-sets")
-    dev = _read_data_set(work_dir, "dev", thread_count, dev_batch_size, numcep, numcontext, limit=limit_dev)
+    test_files = None
+    test_csv = os.path.join(data_dir, "lapsbm-test.csv")
+    if gfile.Exists(test_csv):
+        test_files = pandas.read_csv(test_csv)
 
-    # Create test DataSet
-    test = _read_data_set(work_dir, "test", thread_count, test_batch_size, numcep, numcontext, limit=limit_test)
+    if train_files is None or dev_files is None or test_files is None:
+        print("Processed dataset files not found, downloading and processing data...")
+
+        # Conditionally download data
+        LAPS_DATA = "LapsBM1.4.rar"
+        LAPS_DATA_URL = "http://www.laps.ufpa.br/falabrasil/files/LapsBM1.4.rar"
+        local_file = base.maybe_download(LAPS_DATA, data_dir, LAPS_DATA_URL)
+
+        # Conditionally extract data
+        LAPS_DIR = "LapsBM1.4"
+        _maybe_extract(data_dir, LAPS_DIR, local_file)
+
+        # Aggregate transcripts into a Pandas DataFrame
+        filelist = _aggregate_transcripts(data_dir, LAPS_DIR)
+
+        # Split data into train/validation/test sets
+        train_files, dev_files, test_files = _split_sets(filelist)
+
+        # Write sets to disk as CSV files
+        train_files.to_csv(train_csv, index=False)
+        dev_files.to_csv(dev_csv, index=False)
+        test_files.to_csv(test_csv, index=False)
 
     # Create train DataSet
-    train = _read_data_set(work_dir, "train", thread_count, train_batch_size, numcep, numcontext, limit=limit_train)
+    train = _create_data_set(train_files, thread_count, train_batch_size, numcep, numcontext, limit=limit_train)
+
+    # Create dev DataSet
+    dev = _create_data_set(dev_files, thread_count, dev_batch_size, numcep, numcontext, limit=limit_dev)
+
+    # Create test DataSet
+    test = _create_data_set(test_files, thread_count, test_batch_size, numcep, numcontext, limit=limit_test)
 
     # Return DataSets
     return DataSets(train, dev, test)
@@ -140,54 +162,41 @@ def _maybe_extract(data_dir, extracted_data, archive):
         rar.extractall(data_dir)
         rar.close()
 
-def _maybe_split_sets(data_dir, original_data, converted_data):
-    source_dir = os.path.join(data_dir, original_data)
-    target_dir = os.path.join(data_dir, converted_data)
-    
-    filelist = []
-    
-    # Python 2's glob doesn't support **, so we need os.walk
+def _aggregate_transcripts(data_dir, extracted_data):
+    source_dir = os.path.join(data_dir, extracted_data)
+
+    files = []
     for root, dirnames, filenames in os.walk(source_dir):
+        dirnames.sort()
+        filenames.sort()
         for filename in fnmatch.filter(filenames, "*.txt"):
-            filelist.append(os.path.join(root, filename))
-    
-    filelist = sorted(filelist)
-    
-    # We initially split the entire set into 80% train and 20% test, then
-    # split the train set into 80% train and 20% validation.
+            full_filename = os.path.join(root, filename)
+            wav_filename = os.path.splitext(full_filename)[0] + ".wav"
+            wav_filesize = os.path.getsize(wav_filename)
+            with codecs.open(full_filename, encoding="utf-8") as f:
+                transcript = f.read().strip()
+            transcript = unicodedata.normalize("NFKD", transcript).encode("ascii", "ignore")
+            files.append((wav_filename, wav_filesize, transcript))
+
+    return pandas.DataFrame(data=files, columns=["wav_filename", "wav_filesize", "transcript"])
+
+def _split_sets(filelist):
     train_beg = 0
     train_end = int(0.8 * len(filelist))
-    
+
     dev_beg = int(0.8 * train_end)
     dev_end = train_end
     train_end = dev_beg
-    
+
     test_beg = dev_end
     test_end = len(filelist)
-    
-    _maybe_split_dataset(filelist[train_beg:train_end], os.path.join(target_dir, "train"))
-    _maybe_split_dataset(filelist[dev_beg:dev_end], os.path.join(target_dir, "dev"))
-    _maybe_split_dataset(filelist[test_beg:test_end], os.path.join(target_dir, "test"))
-    
-def _maybe_split_dataset(filelist, target_dir):
-    if not os.path.exists(target_dir):
-        os.makedirs(target_dir)
-        for txt_file in filelist:
-            new_txt_file = os.path.join(target_dir, os.path.basename(txt_file))
-            os.rename(txt_file, new_txt_file)
-            
-            wav_file = os.path.splitext(txt_file)[0] + ".wav"
-            new_wav_file = os.path.join(target_dir, os.path.basename(wav_file))
-            os.rename(wav_file, new_wav_file)
 
-def _read_data_set(work_dir, data_set, thread_count, batch_size, numcep, numcontext, limit=0):
-    # Create data set dir
-    dataset_dir = os.path.join(work_dir, data_set)
-    
-    # Obtain list of txt files
-    txt_files = glob(os.path.join(dataset_dir, "*.txt"))
+    return filelist.iloc[train_beg:train_end], filelist.iloc[dev_beg:dev_end], filelist.iloc[test_beg:test_end]
+
+def _create_data_set(filelist, thread_count, batch_size, numcep, numcontext, limit=0):
+    # Optionally apply dataset size limit
     if limit > 0:
-        txt_files = txt_files[:limit]
-    
+        filelist = filelist.iloc[:limit]
+
     # Return DataSet
-    return DataSet(txt_files, thread_count, batch_size, numcep, numcontext)
+    return DataSet(filelist, thread_count, batch_size, numcep, numcontext)
